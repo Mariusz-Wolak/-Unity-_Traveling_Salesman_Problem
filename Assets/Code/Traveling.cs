0588724f731b3aec08589b50a07b533d834d6c35
Mariusz-Wolak <Mariusz.Wolak@yahoo.com>
2019-05-09 19:18:41 +0200

    added simple traveling code

---
+++ Assets/Code/Traveling.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Traveling : MonoBehaviour
+{
+    [SerializeField]
+    List<Checkpoint> myCheckpoints;
+
+    NavMeshAgent myNavMeshAgent;
+    int currentCheckpointIndex;
+    bool traveling;
+
+    public void Start()
+    {
+        myNavMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (myNavMeshAgent == null)
+        {
+            Debug.LogError("NavMeshAgent component isn't attached to " + gameObject.name);
+        }
+        else
+        {
+            if (myCheckpoints != null && myCheckpoints.Count >= 2)
+            {
+                currentCheckpointIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Need more checkpoints");
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (traveling && myNavMeshAgent.remainingDistance <= 2.0f)
+        {
+            traveling = false;
+
+            NextCheckpoint();
+
+            if (!myNavMeshAgent.isStopped)
+            {
+                SetDestination();
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (myCheckpoints != null)
+        {
+            Vector3 targetVector = myCheckpoints[currentCheckpointIndex].transform.position;
+            myNavMeshAgent.SetDestination(targetVector);
+            traveling = true;
+        }
+    }
+
+    private void NextCheckpoint()
+    {
+        currentCheckpointIndex++;
+
+        if (currentCheckpointIndex >= myCheckpoints.Count)
+        {
+            myNavMeshAgent.isStopped = true;
+        }
+    }
+
+}
+
